8e1f4327d1d90d2b76720fe58c35a5340d26c71d
Mitchell <mitchiemt11@gmail.com>
2023-03-14 15:07:39 +0200

    Add generator slash command

---
+++ app/Http/Controllers/GeneratorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeneratorController extends Controller
+{
+   public function generator(Request $request) {
+    echo "Somethingj...";
+   }
+}
